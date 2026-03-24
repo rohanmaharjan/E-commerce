56a58eb02e18ae1f650bbb500721aefe43f324d1
Rohan Maharjan <rohanmhj9@gmail.com>
2026-03-24 15:00:55 +0545

    added navlinks to navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,18 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
 import Collection from './pages/collection'
 import About from './pages/about'
+import Contact from './pages/contact'
+import Product from './pages/product'
+import Cart from './pages/cart'
+import Login from './pages/login'
+import PlaceOrder from './pages/placeorder'
+import Orders from './pages/orders'
+import NavBar from './components/NavBar'
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <NavBar />
       <Routes>
         <Route path='' element={<Home/>} />
         <Route path='/collection' element={<Collection/>} />
