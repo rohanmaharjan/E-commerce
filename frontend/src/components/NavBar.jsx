326b4e284bffaeb58e98d42a0cb14b5e5b2ad244
Rohan Maharjan <rohanmhj9@gmail.com>
2026-03-24 15:09:52 +0545

    added profile icon and hover

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -30,6 +30,16 @@ const NavBar = () => {
       </ul>
       <div className='flex item-center gap-6'>
         <img src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
+        <div className='group relative'>
+            <img src={assets.profile_icon} className='w-5 cursor-pointer' alt="" />
+            <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+                <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
+                    <p className='cursor-pointer hover:text-black'>My Profile</p>
+                    <p className='cursor-pointer hover:text-black'>Orders</p>
+                    <p className='cursor-pointer hover:text-black'>Logout</p>
+                </div>
+            </div>
+        </div>
       </div>
     </div>
   )
