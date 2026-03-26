fec208376f8422a7e997117f33df13f7da89ba54
Rohan Maharjan <rohanmhj9@gmail.com>
2026-03-26 14:13:47 +0545

    added title latest collection

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,14 +1,20 @@
 import React, { useContext } from 'react'
 import { ShopContext } from '../context/ShopContext'
+import Title from './Title';
 
 const LatestCollection = () => {
 
     const { products } = useContext(ShopContext);
-
     console.log(products);
 
   return (
-    <div>
+    <div className='my-10'>
+      <div className='text-center py-8 text-3xl'>
+        <Title text1={'Latest'} text2={'Collection'} />
+        <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+          Lorem Epsum is simply dummy and printing and typesetting industry.
+        </p>
+      </div>
       
     </div>
   )
