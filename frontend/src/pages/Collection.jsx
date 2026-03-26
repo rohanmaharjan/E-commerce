159e3d4a982357249be9a14d18d75ee32b63b1eb
Rohan Maharjan <rohanmhj9@gmail.com>
2026-03-26 21:37:35 +0545

    added filter

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext'
 
 const Collection = () => {
+
+const { products } = useContext(ShopContext);
+
   return (
-    <div>
+    <div className='flex-flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
+      {/* filter option */}
+      <div className='min-w-60'></div>
+      <p className='my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS</p>
       
     </div>
   )
