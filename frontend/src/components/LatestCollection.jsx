cdc492a6346755c7b5bbb70dc779c2c3d68c4ae9
Rohan Maharjan <rohanmhj9@gmail.com>
2026-03-26 14:35:44 +0545

    displayed latest collection

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,11 +1,20 @@
 import React, { useContext } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from './Title';
+import ProductItem from './ProductItem';
 
 const LatestCollection = () => {
 
     const { products } = useContext(ShopContext);
-    console.log(products);
+
+    // const [ latestProducts , setLatestProducts ] = useState([]);
+
+    // useEffect(()=>{
+    //   setLatestProducts(products.slice(0,10));
+
+    // },[])
+      // derive directly instead of storing in state
+  const latestProducts = products.slice(0, 12);
 
   return (
     <div className='my-10'>
@@ -15,6 +24,15 @@ const LatestCollection = () => {
           Lorem Epsum is simply dummy and printing and typesetting industry.
         </p>
       </div>
+
+    {/* rendering products */}
+    <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid cols-5 gap-4 gap-y-6'>
+      {
+        latestProducts.map((item,index)=>(
+          <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price}/>
+         ))
+      }
+    </div>
       
     </div>
   )
