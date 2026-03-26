994723aaa18f21f5fcca4cab212812053a989d13
Rohan Maharjan <rohanmhj9@gmail.com>
2026-03-26 22:12:52 +0545

    added types of clothwear

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -11,7 +11,9 @@ console.log(products);
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
       {/* filter option */}
       <div className='min-w-60'>
-        <p onClick={() => setShowFilter(!showFilter)} className='my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS</p>
+        <p onClick={() => setShowFilter(!showFilter)} className='my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS
+          
+        </p>
         {/* category filter */}
         <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? '' :'hidden'}`}>
           <p className='mb-3 text-sm font-medium'>CATEGORIES</p>
@@ -27,6 +29,23 @@ console.log(products);
               </p>
             </div>
           </div>
+
+          {/* sub category filter */}
+          <div className={`border border-gray-300 pl-5 py-3 my-5 ${showFilter ? '' :'hidden'}`}>
+          <p className='mb-3 text-sm font-medium'>TYPES</p>
+            <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
+              <p className='flex gap-2'>
+                <input className='w-3' type="checkbox" value={'Topwear'} />Topwear
+              </p>
+              <p className='flex gap-2'>
+                <input className='w-3' type="checkbox" value={'Bottomwear'} />Bottomwear
+              </p>
+              <p className='flex gap-2'>
+                <input className='w-3' type="checkbox" value={'Winterwear'} />Winterwear
+              </p>
+            </div>
+          </div>
+
       </div>
       
     </div>
