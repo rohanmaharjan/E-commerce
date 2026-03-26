5cf3e5866fef6d18f5b2c3714e2fa6dd73b9058d
Rohan Maharjan <rohanmhj9@gmail.com>
2026-03-26 21:32:37 +0545

    created footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div>
+        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+            <div>
+                <img src={assets.logo} className='mb-4 w-32' alt="" />
+                <p className='w-full md:w-2/3 text-gray-600'>
+                Lorem Ipsum is simply dummy text of the pronitng an dtypesetting industry.
+                </p>
+            </div>
+
+            <div>
+                <p className='text-xl font-medium mb-5'>COMPANY</p>
+                <ul className='flex flex-col gap-1 text-gray-600'>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Delicery</li>
+                    <li>Privacy Policy</li>
+                </ul>
+            </div>
+
+            <div>
+                <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+                <ul className='flex flex-col gap-1 text-gray-600'>
+                    <li>+977 9742875914</li>
+                    <li>contact@forever.com</li>
+                </ul>
+            </div>
+            <div>
+                <hr />
+                <p className='py-5 text-sm text-center'>Copyright 2026.All rights reserved.</p>
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Footer
