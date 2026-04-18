2162c3c1b919dad431321a3de4698e18f8a73b83
Rohan Maharjan <rohanmhj9@gmail.com>
2026-04-18 22:01:01 +0545

    added product description

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,13 +1,15 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
+import { assets } from '../assets/assets'
 
 const Product = () => {
 
 const {productId} = useParams();
-const {products} = useContext(ShopContext);
+const {products,currency} = useContext(ShopContext);
 const [productData,setProductData] = useState(false);
 const [image,setImage] = useState('');
+const [size,setSize] = useState('');
 
 const fetchProductData = async () => {
 
@@ -44,7 +46,29 @@ useEffect (()=>{
             </div>
           </div>
 
-          
+          {/* Product Information */}
+          <div className='flex-1'>
+            <h1 className='font-medium text-2xl mt-2'>{productData.name}</h1>
+            <div className='flex items-center gap-1 mt-2'>
+              <img src={assets.star_icon} alt="" className="w-3 5" />
+              <img src={assets.star_icon} alt="" className="w-3 5" />
+              <img src={assets.star_icon} alt="" className="w-3 5" />
+              <img src={assets.star_icon} alt="" className="w-3 5" />
+              <img src={assets.star_dull_icon} alt="" className="w-3 5" />
+              <p className='pl-2'>(122)</p>
+            </div>
+            <p className='mt-5 text-3xl font-medium'>{currency}{productData.price}</p>
+            <p className='mt-5 text-gray-500 md:w-4/5'>{productData.description}</p>
+            <div className='flex flex-col gap-4 my-8'>
+              <p>Select Size</p>
+              <div className='flex gap-2'>
+                {productData.sizes.map((item,index)=>(
+                  <button onClick={()=>setSize(item)} className={`border py-2 px-4 bg-gray-100 ${item === size ? 'border-orange-500' : ''}`} key={index}>{item}</button>
+                ))}
+              </div>
+            </div>
+            
+          </div>
         </div>
       </div>
       
